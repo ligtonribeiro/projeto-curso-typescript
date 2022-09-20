4941ac0d1e20a95741feb1d8e9b69fb6e0ee01ad
Wellington Menezes <ligton.ribeiro@gmail.com>
2022-09-20 12:32:28 -0300

    finalização do curso Typescript módulo 2

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,11 @@ import { NegociacoesView } from './views/negociacoes-view.js';
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  controller.adiciona();
-});
+if (form) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    controller.adiciona();
+  });
+} else {
+  throw new Error('Não foi possível inicializar o app. Verifique se o Form existe.');
+}
--- app/views/view.ts
+++ app/views/view.ts
@@ -1,11 +1,24 @@
 export abstract class View<T> {
     protected elemento: HTMLElement;
+    private escapar = false;
 
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
+    constructor(seletor: string, escapar?: boolean) {
+        const elemento = document.querySelector(seletor); 
+        if (elemento) {
+            this.elemento = elemento as HTMLInputElement;
+        } else {
+            throw new Error(`Seletor ${seletor} não existe no DOM`);
+        }
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
 
     public update(model: T): void {
+        let template = this.template(model)
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.elemento.innerHTML = this.template(model);
     }
 
